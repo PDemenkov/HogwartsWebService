4db69c10e3c06440b68b10ae0db68052ed59bbf1
PDemenkov <guapnik@gmail.com>
2022-12-07 15:35:04 +0300

    f

---
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -0,0 +1,16 @@
+package ru.hogwarts.school.model;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
